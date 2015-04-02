f9e2ed0421c5fab8d3fe9d3cc73187a191c9409b
Henrique Silveira <hd2312@columbia.edu>
2015-04-01 21:30:39 -0300

    Adicionamos funcoes para threads de mdr, ir, memoria, banco de registradores, a, b

--- codigo/include/src/threads.c
+++ codigo/include/src/threads.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* 	O Mux precisa do sinal de controle para decidir qual dado usará.
-	Consequentemente, o Mux precisa de só um buffer de entrada.  
+/*  O Mux precisa do sinal de controle para decidir qual dado usará.
+  Consequentemente, o Mux precisa de só um buffer de entrada.  
 */
 
 //////////////// VARIAVEIS GLOBAIS: BUFFER DE MUXES /////////
@@ -12,18 +12,30 @@ int mux_ALUSrcA_buffer;
 int mux_ALUSrcB_buffer;
 //////////////// FIM VARIAVEIS GLOBAIS: BUFFER DE MUXES /////////
 
+
+//// TEMOS QUE PENSAR EM UM JEITO DE ENCERRAR AS THREADS QUANDO O
+//// PROGRAMA NA MEMORIA CHEGA AO FIM. OU SEJA: TEMOS QUE INCLUIR
+//// WHILES NAS THREADS PARA QUE ELAS CONTINUEM EXECUTANDO ATE QUE
+//// UMA CONDICAO DE PARADA SEJA VERIFICADA. PODEMOS FAZER ISSO USANDO
+//// UMA VARIAVEK GLOBAL QUE COMECA SENDO VERDADEIRA E VIRA FALSA QUANDO
+//// UMA INSTRUCAO NULA EH ENCONTRADA NA MEMORIA, INDICANDO O FIM DO 
+//// PROGRAMA. ESSA VARIAVEL EH CONSTANTEMENTE CHECADA PELAS THREADS
+
 ///////// VARIAVEIS GLOBAIS: COLOCAR NA MAIN //////////
 // -> sc ja esta na main
-// pc
+// int pc
 // aluout
 // mdr
 // IR
-// A
-// B
+// int A
+// int B
 // -> alu_result
 // -> jump_address
 // FOUR = 0x00000004
 // BEQ_Address
+// memory_content_read
+// read_data_1
+// read_data_2
 
 //////////////////threads faltando////////////////
 // shift left2 (as duas [em um deles deve ter concatenacao com 4 bits de PC])
@@ -32,11 +44,11 @@ int mux_ALUSrcB_buffer;
 // A (deve ter uma thread para escrever aqui)
 // B (deve ter uma thread para escrever aqui)
 // AND e OR
-// MDR (deve ter uma thread para escrever aqui)
-// PC (deve ter uma thread para escrever aqui)
-// Memory
+// OK--------> MDR (deve ter uma thread para escrever aqui)
+// OK--------> PC (deve ter uma thread para escrever aqui)
+// OK--------> Memory
 // Banco de Registradores
-// IR
+// OK--------> IR
 // ALUout
 // ALUcontrol
 
@@ -64,42 +76,42 @@ enum positions {
 void mux_2_IorD (){
   //mutex lock
     if(( (separa_IorD & sc) >> IorD_POS) & 0x01 == 0)
-    	mux_IorD_buffer = pc;
+      mux_IorD_buffer = pc;
     else 
-    	mux_IorD_buffer = aluout;
+      mux_IorD_buffer = aluout;
 }
 void mux_2_MemtoReg(){
   //mutex lock
     if(( (separa_MemtoReg & sc) >> MemtoReg_POS) & 0x01 == 0)
-    	mux_MemtoReg_buffer = aluout;
+      mux_MemtoReg_buffer = aluout;
     else 
-    	mux_MemtoReg_buffer = mdr;
+      mux_MemtoReg_buffer = mdr;
 }
 
 void mux_2_RegDst (){
   //mutex lock
     if(( (separa_RegDst & sc) >> RegDst_POS) & 0x01 == 0)
-    	mux_RegDst_buffer = ((IR & separa_rt) >> 16) & 0x0000001f;
+      mux_RegDst_buffer = ((IR & separa_rt) >> 16) & 0x0000001f;
     else 
-    	mux_RegDst_buffer = ((IR & separa_rd) >> 11) & 0x0000001f;
+      mux_RegDst_buffer = ((IR & separa_rd) >> 11) & 0x0000001f;
 }
 
 void mux_2_ALUSrcA (){
   //mutex lock
     if(( (separa_ALUSrcA & sc) >> ALUSrcA_POS) & 0x01 == 0)
-    	mux_ALUSrcA_buffer = pc;
+      mux_ALUSrcA_buffer = pc;
     else 
-    	mux_ALUSrcA_buffer = A;
+      mux_ALUSrcA_buffer = A;
 }
 
 void mux_3_PCSoure (){
   //mutex lock
     if((((separa_PCSource0 | separa_PCSource1) & sc) >> PCSource0_POS) & 0x03 == 0)
-    	mux_PCSource_buffer = alu_result;
+      mux_PCSource_buffer = alu_result;
     else if((((separa_PCSource0 | separa_PCSource1) & sc) >> PCSource0_POS) & 0x03 == 1)
-    	mux_PCSource_buffer = aluout;
+      mux_PCSource_buffer = aluout;
     else
-      	mux_PCSource_buffer = jump_address;
+        mux_PCSource_buffer = jump_address;
 }
 
 void mux_4_ALUSrcB (){
@@ -107,11 +119,65 @@ void mux_4_ALUSrcB (){
   if((((separa_ALUSrcB0 | separa_ALUSrcB1) & sc) >> ALUSrcB0_POS) & 0x03 == 0)
     mux_ALUSrcB_buffer = B;
   else if((((separa_ALUSrcB0 | separa_ALUSrcB1) & sc) >> ALUSrcB0_POS) & 0x03 == 1)
-    	mux_ALUSrcB_buffer = FOUR;
-  	else if((((separa_ALUSrcB0 | separa_ALUSrcB1) & sc) >> ALUSrcB0_POS) & 0x03 == 2)
-    	mux_ALUSrcB_buffer = BEQ_Adress;
+      mux_ALUSrcB_buffer = FOUR;
+    else if((((separa_ALUSrcB0 | separa_ALUSrcB1) & sc) >> ALUSrcB0_POS) & 0x03 == 2)
+      mux_ALUSrcB_buffer = BEQ_Adress;
     else
-      	mux_ALUSrcB_buffer = BEQ_Adress << 2;  
+        mux_ALUSrcB_buffer = BEQ_Adress << 2;  
 }
+
+void escreve_PC (){
+  //mutex lock de PC e PCSource_buffer
+  // INCLUIR CHECAGEM DO SINAL PARA VERIFICAR SE ESCREVE NO PC OU NAO
+  // VAI SER NECESSARIO CRIAR UM NOVO SINAL RESULTANTE DOS SINAIS DE CONTROLE
+  // PASSANDO PELAS PORTAS AND E OR
+  // FAZER ISSO APOS IMPLEMENTAR A ULA, POIS O SINAL ZERO EH NECESSARIO
+  pc = mux_PCSource_Buffer; 
+}
+
+void escreve_MDR (){
+  //mutex lock de PC e PCSource_buffer
+  mdr = memory_content_read;  
+}
+
+void escreve_IR (){
+  if ((sc & separa_IRWrite) == ativa_IRWrite)
+    IR = memory_content_read; 
+}
+
+void escreve_A (){
+  //mutex lock de PC e PCSource_buffer
+  A = read_data_1;  
+}
+
+void escreve_B (){
+  //mutex lock de PC e PCSource_buffer
+  B = read_data_2;  
+}
+
+void memory_access ()
+{
+    if ((separa_MemRead & sc) == ativa_MemRead)  
+      memory_content_read = memoria[mux_IorD_buffer/4];
+  
+    if ((sc & separa_MemWrite) == ativa_MemWrite)
+      memoria[Adress/4] = B;
+}
+
+void banco_registradores ()
+{
+  int read_register_1, read_register_2, write_register, write_data;
+    read_register_1 = ((separa_rs & IR) >> 21) & 0x0000003f;
+    read_register_2 = ((separa_rt & IR) >> 16) & 0x0000003f;
+    read_data_1 = reg[read_register_1];
+    read_data_2 = reg[read_register_2];
+    write_register = mux_RegDst_buffer;
+  write_data = mux_MemtoReg_buffer;
+  
+    if ((sc & separa_RegWrite) == ativa_RegWrite)
+      reg[write_register] = write_data;
+  
+}
+
 
 
