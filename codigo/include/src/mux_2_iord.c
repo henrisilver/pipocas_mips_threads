88d4b4964709481a13fe305e8c20a2a19c396d0a
Marcello Costa <marcellopfcosta@gmail.com>
2015-04-06 20:01:37 -0300

    Mux_2_IorD.c correto

--- codigo/include/src/mux_2_iord.c
+++ codigo/include/src/mux_2_iord.c
@@ -10,53 +10,37 @@
 #include "mascara.h"
 
 #define PC 0
-extern int pc,aluout,cpu_clock;
 
-pthread_mutex_t control_sign;
-pthread_cond_t cs_ready;		// Should this condition be declared in control unit scope?
+/* Extern global variables  */
+extern int cpu_clock;
+extern int pc_value;
+extern int aluout_value
+extern c_sign cs;
 
-int mux_IorD_buffer;			// This must be declared as "extern int mux_IorD_buffer" in the file that will include present file.
+extern pthread_mutex_t control_sign;
+extern pthread_cond_t control_sign_ready;
 
-/*
-	Precisamos pensar em como executar cada ciclo independentemente. Discutimos que deveria ser empregado o uso de barreiras.
-	>> pthread_barrier_init(barrier,attr,count);
+/* This file's global variable  */
+int mux_IorD_buffer;
 
-	Imaginei o seguinte: pthread_barrier_init deve ser chamado da main, esta que dita o clock. Quando é criado uma barreira, as threads que	depende desta devem executar o comando pthread_barrier_wait(barrier), cada thread que executar esse comando aumenta um contador da barreira. Enquanto esse número for menor do que count, as threads não executam.
-
-	Para isso funcionar como controle de clock, quando a thread executar a tarefa, ela libera o mutex ou coisa parecida e chama a função de esperar pela barreira antes de executar pthread_yield();
-
-	Uma pequena dúvida que tenho e gostaria de saber o que acham: Devemos pensar em quais sincronizações entre as threads? Digo, quais precisam executar primeiro? Exceto a unidade de controle que é obvio, temos que pensar em outras sincronias importantes ou é independente da sequência de execução das unidades lógicas? É mais natural pensar que precisamos de outras sincronias, mas isso não estaria tornando a concorrência desnecessária ou algo do tipo? Dorgas, mano.
- 
-*/
-
-
-// Este multiplexador não dependerá da execução de outras threads em um ciclo
 void mux_2_IorD(){
-	int last_clock = 10; 		// Local clock to keep track if thread has already executed.
+	int last_clock = 10;
 
-	while(valid_instruction){
+	while(ir_value){
 		if (last_clock != cpu_clock){
+
 			pthread_mutex_lock(&control_sign);
-			// This thread's routine won't go any further whilst mutex isn't locked.
+			if(!cs.isUpdated)
+				while(pthread_cond_wait(&cs_ready,&control_sign) != 0);
+                        pthread_mutex_unlock(&control_sign);
 
-			while(pthread_cond_wait(&cs_ready,&control_sign) != 0);
-			// pthread_cond_wait returns 0 upon successful completion.
-			// While it doesn't receive the broadcast from control unit, it won't classify as a successful routine.
-
-			// Both the mutex is locked and control unit has successfully broadcasted its sign.
-			// From now on, threads associated with MIPS CPU can execute accordingly.
 			if(( (separa_IorD & sc) >> IorD_POS) & 0x01 == PC){
-      				mux_IorD_buffer = pc;
+      				mux_IorD_buffer = pc_value;
 			}
-			else{
-				mux_IorD_buffer = aluout;
-			}
+			else mux_IorD_buffer = aluout_value;
+
 			last_clock = cpu_clock;
-			// Flag that tells which clock was running when the job was completed
-	
-			pthread_mutex_unlock(&control_sign);
-			pthread_barrier_wait(&current_cycle);		// Ilustrando o raciocínio acima de controle de ciclo
-			// Unlocks mutex and yields so another thread can do its job
+			pthread_barrier_wait(&current_cycle);
 		}
 		else pthread_yield();
 	}
