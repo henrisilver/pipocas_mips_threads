7b7767cf32f9a5d941cd7a05636c9ed5a59bac42
Henrique Silveira <hd2312@columbia.edu>
2015-04-06 23:03:48 -0300

    Control Unit parece estar nos trinques. Vamos torcer para que funcione

--- codigo/include/src/control_unit.c
+++ codigo/include/src/control_unit.c
@@ -1,10 +1,12 @@
 /*
-    File with implementation of the control unit routine.
-    It receives a control parameter from control unit to choose
-    between PC's or ALUOut's adresses content to send to the main memory.
-*/
-#ifndef _UNIDADE_CONTROLE_
-#define _UNIDADE_CONTROLE_
+ File with implementation of the control unit routine.
+ It receives a control parameter from control unit to choose
+ between PC's or ALUOut's adresses content to send to the main memory.
+ */
+#ifndef _CONTROL_UNIT_
+#define _CONTROL_UNIT_
+
+#define NUM_OF_THREADS_WHICH_USE_CSVALUE 11
 
 #include <pthread.h>
 #include "mascara.h"
@@ -13,7 +15,7 @@
 extern int pc,aluout,cpu_clock;
 
 pthread_mutex_t control_sign;
-pthread_cond_t cs_ready;        // Should this condition be declared in control unit scope?
+pthread_cond_t control_sign_wait;        // Should this condition be declared in control unit scope?
 
 
 /////////////////////////////////////////////////////////////////////
@@ -53,7 +55,7 @@ void gera_sinal_controle (int *output, char S)
         *output = *output | ativa_ALUSrcB1;
         *output = *output & desativa_ALUOp0;
         *output = *output & desativa_ALUOp1;
-    
+        
         *output = *output | tab_desp1;
     }
     
@@ -140,66 +142,81 @@ void gera_sinal_controle (int *output, char S)
         *output = *output | ativa_PCWrite;
         *output = *output & desativa_PCSource0;
         *output = *output | ativa_PCSource1;
-        
         *output = *output | volta_busca;
     }
 }//fecha void gera_sinal_controle (int *output, char S)
 
-void control_unit(int IR, short int *sc, int cycle)
+typedef struct c_sign{
+    short int value;        // Inteiro que representa o sinal de controle
+    int isUpdated;          // 1 para atualizado e 0 caso contrário
+}c_sign;
+
+c_sign cs;
+
+void control_unit(void *not_used)
 {
-  int sinal;
+    int sinal;
     char S;
-    char op = ((IR & separa_cop) >> 26) & 0x3f;
-  switch(cycle)
+    char op = ((ir & separa_cop) >> 26) & 0x3f;
+    while(ir)
     {
-        case 0:
-          /*Busca Instrução 1 Estado
-              Gera sinais de controle
-            */
-          S = 0;
-        break;
-      
-        case 1:
-          /*Decodifica 2 Estados
-              Gera novos sinais de controle
-            */
-          S = 1;
-        break;
-      
-        case 2:
-          /* Dependererá do IR. IFS?
-            if(branch) Gera sinais p branch
-      else if(add) gera sinais p add
-            add,sub,lw,sw
-            */
-          if(op == op_jump)//jump
-              S = 9;
-            else if(op == op_beq)//beq
-              S = 8;
-            else if(op == op_r_type)//r-type
-              S = 6;
-            else if(op == op_lw || op == op_sw)//lw ou sw
-              S = 2;
-        break;
-      
-        case 3:
-          /*
+        if (last_clock != cpu_clock){
+            switch(cpu_clock)
+            {
+                case 0:
+                    /*Busca Instrução 1 Estado
+                     Gera sinais de controle
+                     */
+                    S = 0;
+                    break;
+                    
+                case 1:
+                    /*Decodifica 2 Estados
+                     Gera novos sinais de controle
+                     */
+                    S = 1;
+                    break;
+                    
+                case 2:
+                    if(op == op_jump)                   //jump
+                        S = 9;
+                    else if(op == op_beq)               //beq
+                        S = 8;
+                    else if(op == op_r_type)            //r-type
+                        S = 6;
+                    else if(op == op_lw || op == op_sw) //lw ou sw
+                        S = 2;
+                    break;
+                    
+                case 3:
+                    if(op == op_r_type)     //r-type
+                        S = 7;
+                    else if(op == op_lw)    //lw ou sw
+                        S = 3;
+                    else if(op == op_sw)    //lw ou sw
+                        S = 5;
+                    break;
+                    
+                case 4: // Somente executara no caso da instrucao lw
+                    S = 4;
+                    break;
+            }
+            gera_sinal_controle (&sinal, S);
             
-            */
-            if(op == op_r_type)//r-type
-              S = 7;
-            else if(op == op_lw) //lw ou sw
-              S = 3;
-            else if(op == op_sw) //lw ou sw
-              S = 5;
-        break;
-      
-        case 4: // Somente executara no caso da instrucao lw
-          S = 4;
-        break;
+            pthread_barrier_init(&current_cycle,NULL, (unsigned int)NUM_OF_THREADS_WHICH_USE_CSVALUE);
+            pthread_mutex_lock(&control_sign);
+            cs.value = (short int)(sinal & 0x0000ffff);
+            cs.updated = 1;
+            pthread_cond_broadcast(&control_sign_wait);
+            pthread_mutex_unlock(&control_sign);
+            pthread_barrier_wait(&current_cycle);
+            
+            cs.updated = 0;
+            
+            pthread_barrier_wait(&update_registers);
+        }
+        else pthread_yield();
     }
-    gera_sinal_controle (&sinal, S);
-    *sc = output_signals & 0x0000ffff;
-    // broadcast(sc);
+    pthread_exit(0);
 }
 #endif
