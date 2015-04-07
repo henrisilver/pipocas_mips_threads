146885a46e312e07bde65f8a2ce80839305d0cd8
Henrique Silveira <hd2312@columbia.edu>
2015-04-07 09:05:35 -0300

    Adicionado arquivo para a alu

---
+++ codigo/include/src/alu.c
@@ -0,0 +1,10 @@
+/*
+	File with implementation of ALU routine.
+	It receives a control parameter from the ALU control unit which
+	indicates what kind of operation the ALU should perform.
+*/
+#ifndef _MUX_2_MEMTOREG_
+#define _MUX_2_MEMTOREG_
+
+#include <pthread.h>
+#include "mascara.h"
