b416a9e1c0728b87a01cb0febf69c2799059b180
Marcello Costa <marcellopfcosta@gmail.com>
2015-04-08 16:41:53 -0300

    veeeeeeeeeeeeei ir!

--- codigo/include/src/instruction_register.c
+++ codigo/include/src/instruction_register.c
@@ -17,6 +17,7 @@ int ir;
 
 extern pthread_mutex_t control_sign;
 extern pthread_cond_t control_sign_wait;
+
 extern pthread_barrier_t current_cycle;
 extern pthread_barrier_t update_registers;
 
@@ -26,19 +27,16 @@ void Instruction_Register(){
 
         while(ir){
                 if (last_clock != cpu_clock){
-
                         pthread_mutex_lock(&control_sign);
                         if(!cs.isUpdated)
                                 while(pthread_cond_wait(&control_sign_wait,&control_sign) != 0);
                         pthread_mutex_unlock(&control_sign);
-
                         last_clock = cpu_clock;
-                        pthread_barrier_wait(&current_cycle);
 
+                        pthread_barrier_wait(&current_cycle);
 			if(( (separa_IRWrite & cs.value) >> IRWrite_POS) & 0x01 == IRWrite){
                                 ir = memory_content_read;
                         }
-
 			pthread_barrier_wait(&update_registers);
                 }
                 else pthread_yield();
