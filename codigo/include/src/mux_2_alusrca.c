85f4d802949b9809ea1c13c9b4dd70b0a1794862
GuilhermeNishina <xwe116@gmail.com>
2015-04-20 21:43:49 -0300

    Update mux_2_alusrca.c

--- codigo/include/src/mux_2_alusrca.c
+++ codigo/include/src/mux_2_alusrca.c
@@ -1,3 +1,16 @@
+/* SSC0640 - Sistemas Operacionais I
+* Trabalho 1 - CPU MIPS com pthreads em C
+* Data: 22/04/2015
+***************************************************************
+* Integrantes do Grupo:
+* Guilherme Nishina Fortes             - No. USP 7245552
+* Henrique de Almeida Machado da Silveira     - No. USP 7961089
+* Marcello de Paula Ferreira Costa         - No. USP 7960690
+* Sergio Yudi Takeda                 - No. USP 7572996
+***************************************************************
+* GRUPO 13
+*/
+
 /* multiplexador de duas entradas com sinal de controle denominado de ALUSrcA que seleciona qual dado entre o registrador A ou
  * o valor de pc que entrara na ula. Usualmente os outros multiplexadores de 2, 3 e 4 entradas serao denominados de maneira similar
  * com mux_"numentradas"_"sinalcontrole" sendo a regra. */
